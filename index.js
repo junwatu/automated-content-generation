56b64b9e9569e9c4fea7e539bca93eabf3374f51
Equan P <equan.p@gmail.com>
2023-05-17 15:13:07 +0700

    update the server

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { generateRoute } from './generate.js';
-import { imageRoute } from './image.js';
+import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv'
+import { generateRoute } from './routes/generate.js';
+
+dotenv.config()
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json())
 app.use('/api/generate', generateRoute);
-app.use('/api/image', imageRoute);
+
 app.get("/", async function (req, res) {
 	res.json({ app: "automated content generation" })
 })
