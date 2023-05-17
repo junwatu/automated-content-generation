54126b5e669826a310e9fef33f03649cc5fcd448
Equan P <equan.p@gmail.com>
2023-05-17 17:00:55 +0700

    add image route

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from 'express'
+import bodyParser from 'body-parser'
 import * as dotenv from 'dotenv'
-import { generateRoute } from './routes/generate.js';
+
+import { generateRoute } from './routes/generate.js'
+import { imageRoute } from './routes/image.js'
 
 dotenv.config()
 
@@ -9,11 +11,12 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json())
-app.use('/api/generate', generateRoute);
+app.use('/api/generate', generateRoute)
+app.use('/api/image', imageRoute)
 
 app.get("/", async function (req, res) {
 	res.json({ app: "automated content generation" })
 })
 app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
+	console.log(`Server running at http://localhost:${port}`)
 });
