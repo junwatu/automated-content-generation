4e7fd184f964594363f0beb587dbbe887282d8fe
Equan P <equan.p@gmail.com>
2023-05-17 15:10:41 +0700

    move into routes dir

--- generate.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-export const generateRoute = express.Router();
-
-generateRoute.get('/', (req, res) => {
-	res.json({ message: 'Generate route' });
-});
---
+++ routes/generate.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import { OpenAIApi, Configuration } from "openai"
+import { } from "../libs/imageai"
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export function createContentPrompt(imagine) {
+	if (!imagine) {
+		throw new Error("User request is required to create a content prompt.");
+	}
+
+	return `Make a short blog of fewer than 500 words with this user request:
+    \n
+    ${imagine}
+    \n
+    The answer should be a JSON only with the format {title: [insert_title], content: [insert_content], imageprompt: [summarize_content_for_image_generation]}. Do not answer with the context or any explanation.`;
+}
+
+async function generateContent(imagine) {
+	const prompt = createContentPrompt(imagine)
+	try {
+		const completion = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			max_tokens: 2000,
+			messages: [{ role: "user", content: prompt }],
+		});
+		return completion.data.choices[0].message;
+	} catch (error) {
+		console.error("Error occurred while generating content:", error);
+		return "An error occurred while generating content.";
+	}
+}
+
+export const generateRoute = express.Router();
+
+generateRoute.post('/', async (req, res) => {
+	// receive JSON data
+	const content = await generateContent(req.body.prompt)
+	res.json({ content });
+});
