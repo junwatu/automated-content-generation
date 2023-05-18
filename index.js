f8f44063eaeff7d431491b7d7bd75b2df70269b0
Equan P <equan.p@gmail.com>
2023-05-18 07:56:20 +0700

    init and log griddb

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import * as GridDB from "./libs/griddb.cjs";
 import path, { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-
 import { generateRoute } from './routes/generate.js'
 import { imageRoute } from './routes/image.js'
-
+import { logger } from './libs/logger.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const { collectionDb, store, conInfo } = await GridDB.initGridDbTS();
+
+logger.info(await GridDB.containersInfo(store))
 
 const app = express();
 const port = 3000;
@@ -21,5 +24,5 @@ app.get("/", async function (req, res) {
 	res.json({ app: "automated content generation" })
 })
 app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
+	logger.info(`Server running at http://localhost:${port}`)
 });
